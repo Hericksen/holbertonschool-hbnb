7bdf82a211ba4031b00053c015b743141f307a53
HamzaMoinet <moinethamza0@gmail.com>
2025-02-26 15:29:34 +0100

    modif place

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -17,15 +17,25 @@ user_model = api.model('User', {
     'email': fields.String(description='Email')
 })
 
+# Adding the review model
+review_model = api.model('PlaceReview', {
+    'id': fields.String(description='Review ID'),
+    'text': fields.String(description='Text of the review'),
+    'rating': fields.Integer(description='Rating of the place (1-5)'),
+    'user_id': fields.String(description='ID of the user')
+})
+
 # Modèle pour la validation des données d'un lieu
 place_model = api.model('Place', {
-    'title': fields.String(required=True, description='Titre du lieu'),
-    'description': fields.String(description='Description du lieu'),
-    'price': fields.Float(required=True, description='Prix par nuit (>= 0)'),
-    'latitude': fields.Float(required=True, description='Latitude (entre -90 et 90)'),
-    'longitude': fields.Float(required=True, description='Longitude (entre -180 et 180)'),
-    'owner_id': fields.String(required=True, description='ID du propriétaire'),
-    'amenities': fields.List(fields.String, required=True, description="Liste des IDs des équipements")
+    'title': fields.String(required=True, description='Title of the place'),
+    'description': fields.String(description='Description of the place'),
+    'price': fields.Float(required=True, description='Price per night'),
+    'latitude': fields.Float(required=True, description='Latitude of the place'),
+    'longitude': fields.Float(required=True, description='Longitude of the place'),
+    'owner_id': fields.String(required=True, description='ID of the owner'),
+    'owner': fields.Nested(user_model, description='Owner of the place'),
+    'amenities': fields.List(fields.Nested(amenity_model), description='List of amenities'),
+    'reviews': fields.List(fields.Nested(review_model), description='List of reviews')
 })
 
 @api.route('/')
