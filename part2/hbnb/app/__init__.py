509750c5f9df5ed1d05d7d4984ed0286688c2786
HamzaMoinet <moinethamza0@gmail.com>
2025-03-02 15:20:42 +0100

    del import uuid

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -4,7 +4,6 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
-import uuid
 
 def create_app():
     app = Flask(__name__)
