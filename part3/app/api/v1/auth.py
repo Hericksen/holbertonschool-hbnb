316fd0c2640c476fec49075cc5e7d5f2de0452bc
HamzaMoinet <moinethamza0@gmail.com>
2025-04-03 17:47:42 +0200

    delete json

--- part3/app/api/v1/auth.py
+++ part3/app/api/v1/auth.py
@@ -1,7 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from flask_jwt_extended import create_access_token
 from app.services import facade
-import json
 
 api = Namespace('auth', description='Authentication operations')
 
@@ -26,7 +25,7 @@ class Login(Resource):
             return {'error': 'Invalid credentials'}, 401
 
         # Step 3: Create a JWT token with the user's id and is_admin flag
-        access_token = create_access_token(identity=json.dumps({'id': str(user.id), 'is_admin': user.is_admin}))
+        access_token = create_access_token(identity={'id': str(user.id), 'is_admin': user.is_admin})
 
         # Step 4: Return the JWT token to the client
         return {'access_token': access_token}, 200
--- part3/app/api/v1/protected.py
+++ part3/app/api/v1/protected.py
@@ -1,6 +1,5 @@
 from flask_restx import Namespace, Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
-import json
 
 api = Namespace('protected', description='Protected operations')
 
@@ -9,5 +8,5 @@ class ProtectedResource(Resource):
     @jwt_required()
     def get(self):
         """A protected endpoint that requires a valid JWT token"""
-        current_user = json.loads(get_jwt_identity())  # Convertir la chaîne JSON en dictionnaire
+        current_user = get_jwt_identity()  # Convertir la chaîne JSON en dictionnaire
         return {'message': f'Hello, user {current_user["id"]}'}, 200
